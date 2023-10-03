6f5d1d05beaa8da9513a4170e515eee58be5c3f5
Ondrej Sefcik <Ondrej.Sefcik@student.sps-cl.cz>
2023-10-03 10:29:08 +0200

    oprava předchozí verze

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function fibCislo (n) {
     if (n<2) return 1;
     return fibCislo(n-1) + fibCislo(n-2);
 }
-function vypocitej() {
-    alert(fibCislo(Number(document.getElementById("stup").value)))
+function vypočítej() {
+    alert(fibCislo(Number(document.getElementById("vstup").value)))
 }
